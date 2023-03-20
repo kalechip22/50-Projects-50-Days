1a5842aabcc4c1e0a12d862cf0cd0269bfc27d3e
kalechip22 <kayla.hom22@gmail.com>
2023-03-20 02:27:29 -0700

    done with day 1

--- 1-ExpandingCards/script.js
+++ 1-ExpandingCards/script.js
@@ -1,14 +1,16 @@
-// Selecting all sections with class of section
-const sections = document.querySelectorAll('.section')
-  
-// On click event for each section 
-sections.forEach((section)=>{
-    section.addEventListener('click',()=>{
-    // remove active class from all another section 
-    // and add it to the selected section
-        sections.forEach((section) => {
-            section.classList.remove('active')
-        })
-        section.classList.add('active')
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', ()=>{
+        // Remove active from all other classes
+        removeActiveClasses()
+        // Add active to panel clicked on
+        panel.classList.add('active')
     })
 })
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
