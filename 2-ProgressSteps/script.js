38ee34dae57b70889416c7241127f8daacaf3eba
kalechip22 <kayla.hom22@gmail.com>
2023-03-21 02:29:37 -0700

    finished day 2

--- 2-ProgressSteps/script.js
+++ 2-ProgressSteps/script.js
@@ -1,16 +1,54 @@
-const panels = document.querySelectorAll('.panel')
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
-panels.forEach(panel => {
-    panel.addEventListener('click', ()=>{
-        // Remove active from all other classes
-        removeActiveClasses()
-        // Add active to panel clicked on
-        panel.classList.add('active')
-    })
+let currActive = 1
+
+// Event listener for next button
+next.addEventListener('click', ()=>{
+    currActive++
+    
+    if(currActive > circles.length) {
+        currActive = circles.length
+    }
+
+    update()
 })
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
+// Event listener for prev button
+prev.addEventListener('click', ()=>{
+    currActive--
+    
+    if(currActive < 1) {
+        currActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+        // Adds active class to all active numbers
+        if(idx < currActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
     })
+
+    const actives = document.querySelectorAll('.active')
+
+    // Calculates how long line should be
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    // Disables button based on currently active button
+    if(currActive === 1) {
+        prev.disabled = true
+    } else if(currActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
